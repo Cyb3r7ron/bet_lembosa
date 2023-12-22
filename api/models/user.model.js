7735dad506cc21754c3fda9e6b06e0114b8632ab
Cyb3r7ron <kiduskyle@gmail.com>
2023-12-22 05:36:05 -0800

    finnalized everything

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,27 +1,28 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     username: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
     },
-    email : {
-        type: String,
-        required: true,
-        unique: true,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
-    avatar: {
-     type: String,
-     default:"https://cdn3.iconfinder.com/data/icons/diversity-avatars-vol-2/64/afro-african-man-glasses-512.png"
+    avatar:{
+      type: String,
+      default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
     },
-    // stopped at 3:23:04
-},
-{ timestamps:true });
+  },
+  { timestamps: true }
+);
 
 const User = mongoose.model('User', userSchema);
 
